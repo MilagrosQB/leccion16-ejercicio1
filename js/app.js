6956d0ba1860f3fe523c8af895aaf8fcb70cec88
MilagrosQB <qbmilagros@gmail.com>
2016-08-25 15:04:08 -0500

    Ejercicio1

---
+++ js/app.js
@@ -0,0 +1,4 @@
+	var num1 = parseInt(prompt("Ingresa tu primer número"));
+	var num2 = parseInt(prompt("Ingresa tu segundo número"));
+	var resultado = num1 + num2;
+  	alert("El resultado es " + resultado);
